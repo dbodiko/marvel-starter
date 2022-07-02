8d031f220d35288ad9e8efb2b0d2a3cd45b1fcaa
Bogdan <dbodiko@gmail.com>
2022-07-03 00:47:20 +0300

    Add error boundary. Update service. Upgrade button 'load more'

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,32 +1,52 @@
-import {Component} from "react";
-import MarvelService from "../../services/MarvelService";
-import ErrorMessage from "../errorMessage/ErrorMessage";
-import Spinner from "../spinner/Spinner";
-
+import {Component} from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
-
-class CharList extends Component{
+class CharList extends Component {
 
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemLoading: false,
+        offset: 351,
+        charEnded: false
     }
 
-    marvelService = new MarvelService()
+    marvelService = new MarvelService();
 
     componentDidMount() {
-        this.marvelService.getAllCharacters()
+        this.onRequest();
+    }
+
+    onRequest = (offset) => {
+        this.onCharListLoading();
+        this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
             .catch(this.onError)
     }
 
-    onCharListLoaded = (charList) => {
+    onCharListLoading = () => {
         this.setState({
-            charList,
-            loading: false
+            newItemLoading: true
         })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
+        this.setState(({offset, charList}) => ({
+            charList: [...charList, ...newCharList],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }))
     }
 
     onError = () => {
@@ -37,8 +57,8 @@ class CharList extends Component{
     }
 
     renderItems(arr) {
-        const items = arr.map((item) => {
-            let imgStyle = {'objectFit' : 'cover'}
+        const items =  arr.map((item) => {
+            let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
             }
@@ -52,7 +72,7 @@ class CharList extends Component{
                     <div className="char__name">{item.name}</div>
                 </li>
             )
-        })
+        });
 
         return (
             <ul className="char__grid">
@@ -61,23 +81,26 @@ class CharList extends Component{
         )
     }
 
-
     render() {
 
-        const {charList, loading, error} = this.state
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
 
-        const items = this.renderItems(charList)
+        const items = this.renderItems(charList);
 
-        const errorMessage = error ? <ErrorMessage/> : null
-        const spinner = loading ? <Spinner/> : null
-        const content = !(loading || error) ? items : null
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
 
         return (
             <div className="char__list">
                 {errorMessage}
                 {spinner}
                 {content}
-                <button className="button button__main button__long">
+                <button
+                    className="button button__main button__long"
+                    disabled={newItemLoading}
+                    style={{'display': charEnded ? 'none' : 'block'}}
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/'
     _apiKey = 'apikey=0c3ac07c8639617a58aeb82b028a9b83'
+    _baseOffset = 351
 
     getResource = async (url) => {
         let res = await fetch(url)
@@ -11,10 +12,8 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = async () => {
-        debugger
-
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=351&${this._apiKey}`)
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`)
         return res.data.results.map(this._transformCharacter)
     }
 
